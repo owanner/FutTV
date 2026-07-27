0012466293f62502dd4868d3447cd0ab7a00acad
Wanner Menezes <owannerm@gmail.com>
2026-07-27 18:46:16 -0300

    inserting vercel analyticcs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
 import AppRoutes from "./routes/AppRoutes";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Analytics />
+    </>
+  );
 }
