91e07570b5eb80e28ca10f9aaa66481993a7fc53
Wanner Menezes <owannerm@gmail.com>
2026-07-27 19:16:42 -0300

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,5 @@
 import AppRoutes from "./routes/AppRoutes";
-import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
-  return (
-    <>
-      <AppRoutes />
-      <Analytics />
-    </>
-  );
+  return <AppRoutes />;
 }
